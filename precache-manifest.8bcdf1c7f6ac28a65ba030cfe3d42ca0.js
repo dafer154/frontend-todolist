87a5003dc76defc7cfe507b8c5a21d20ac56e096
davidfza17 <zuluagadaviding@gmail.com>
2020-08-29 19:21:27 -0500

    Updates

---
+++ precache-manifest.8bcdf1c7f6ac28a65ba030cfe3d42ca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34be3b1b0fc8ab911f1e8a6ad37d5e8",
+    "url": "/frontend-todolist/index.html"
+  },
+  {
+    "revision": "90c422f329efb3165f48",
+    "url": "/frontend-todolist/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c191f1ce9f0986b1d091",
+    "url": "/frontend-todolist/static/css/main.58328112.chunk.css"
+  },
+  {
+    "revision": "90c422f329efb3165f48",
+    "url": "/frontend-todolist/static/js/2.84cd78cf.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/frontend-todolist/static/js/2.84cd78cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c191f1ce9f0986b1d091",
+    "url": "/frontend-todolist/static/js/main.1dbbcdfa.chunk.js"
+  },
+  {
+    "revision": "4278b862c4a4c7d28eb9",
+    "url": "/frontend-todolist/static/js/runtime-main.d80b7f60.js"
+  }
+]);
